ba922d405e250e0b09c9fadb2403f8ef18897985
Martin <martinstojchev@hotmail.com>
2018-10-02 10:05:46 +0200

    Intial setup for the required services

--- ARKitCarGeolocationApp/ViewController.swift
+++ ARKitCarGeolocationApp/ViewController.swift
@@ -8,17 +8,81 @@
 
 import UIKit
 import ARKit
+import SceneKit
+import CoreLocation
+import PusherSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var statusTextView: UITextView!
     
+    let locationManager = CLLocationManager()
+    var userLocation = CLLocation()
+    var modelNode: SCNNode!
+    let rootNodeName = "Car"
+    var originalTransform: SCNMatrix4!
+    var heading: Double! = 0.0
+    
+    var distance: Float! = 0.0 {
+      
+        didSet {
+        setStatusText()
+        }
+    }
+    
+    var status: String! {
+        
+        didSet {
+            setStatusText()
+        }
+    }
+    
+    let pusher = Pusher(
+        key:"9534396ca6dfb8b40428",
+        options: PusherClientOptions(
+            authMethod: .inline(secret: "6a5f535d93487250f10b"),
+            host: .cluster("eu")
+      )
+    )
+    
+    var channel: PusherChannel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set the view's delegate
+        sceneView.delegate = self
+        
+        //Create a new scene
+        let scene = SCNScene()
+        
+        //Set the scene to the view
+        sceneView.scene = scene
+        
+        //Start location services
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        //Set the initial status
+        status = "Getting user location..."
+        
+        //Set a padding in the text view
+        statusTextView.textContainerInset = UIEdgeInsets.init(top: 20.0, left: 10.0, bottom: 10.0, right: 0.0)
+        
+        
+        
+    }
+    
+    func setStatusText(){
+        
+        var text = "Status: \(status)\n"
+        text += "Distance: \(String(format: "%.2f m", distance))"
+        statusTextView.text = text
+        
     }
 
 
