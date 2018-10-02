db9b1b7fa17747a5ec54aba8895bf5e6e2e67db5
Martin <martinstojchev@hotmail.com>
2018-10-02 08:13:46 +0200

    Initial Commit

---
+++ ARKitCarGeolocationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitCarGeolocationApp
+//
+//  Created by Martin on 10/2/18.
+//  Copyright © 2018 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
